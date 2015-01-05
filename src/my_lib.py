fc3d805613574cf0df8d4132a499eabc5e50ab08
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-01-05 20:07:31 +0600

    Add method for formatting matching list

--- src/my_lib.py
+++ src/my_lib.py
@@ -1,5 +1,21 @@
-import re
+import MOODS
 
 
 def pattern_matching_list(text, pattern):
-    return [m.start() for m in re.finditer('(?=' + pattern + ')', text)]
+    # TATA
+    matrix = [[0, 1, 0, 1],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [1, 0, 1, 0]]
+
+    results = MOODS.search(text, [matrix], 3, convert_log_odds=False,
+                           threshold_from_p=False)
+    return list(results)
+
+
+def matching_list_to_string(matching_list):
+    string = ''
+    for i in matching_list:
+        for (position, score) in i:
+            string += "Position: " + str(position) + " Score: " + str(score) + '\n'
+    return string
--- src/search.py
+++ src/search.py
@@ -1,8 +1,8 @@
 import sys
-import MOODS
 
 from Bio import SeqIO
-from my_lib import pattern_matching_list
+
+from my_lib import *
 
 with open(sys.argv[1]) as input_file:
     fasta_seqs = list(SeqIO.parse(input_file, 'fasta'))
@@ -11,22 +11,10 @@ genome = str(fasta_seqs[0].seq)
 pattern = str(fasta_seqs[1].seq)
 
 matching_list = pattern_matching_list(genome, pattern)
-
-# TATA
-matrix = [[0, 1, 0, 1],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [1, 0, 1, 0]]
-
-results = MOODS.search(genome, [matrix], 3, convert_log_odds=False,
-                       threshold_from_p=False)
-
-for i in results:
-    for (position, score) in i:
-        print("Position: " + str(position) + " Score: " + str(score))
+matching_list_str = matching_list_to_string(matching_list)
 
 if len(sys.argv) == 3:
     with open(sys.argv[2], 'w') as output_file:
-        output_file.write(' '.join(map(str, matching_list)) + '\n')
+        output_file.write(matching_list_str)
 else:
-    print ' '.join(map(str, matching_list))
+    print matching_list_str
