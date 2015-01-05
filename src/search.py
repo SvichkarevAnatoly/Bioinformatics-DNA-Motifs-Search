f78cd493a4dc8fd9bb9765c28286783047252df7
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-01-05 19:44:05 +0600

    Search using MOOD lib

--- src/search.py
+++ src/search.py
@@ -1,4 +1,5 @@
 import sys
+import MOODS
 
 from Bio import SeqIO
 from my_lib import pattern_matching_list
@@ -10,6 +11,19 @@ genome = str(fasta_seqs[0].seq)
 pattern = str(fasta_seqs[1].seq)
 
 matching_list = pattern_matching_list(genome, pattern)
+
+# TATA
+matrix = [[0, 1, 0, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [1, 0, 1, 0]]
+
+results = MOODS.search(genome, [matrix], 3, convert_log_odds=False,
+                       threshold_from_p=False)
+
+for i in results:
+    for (position, score) in i:
+        print("Position: " + str(position) + " Score: " + str(score))
 
 if len(sys.argv) == 3:
     with open(sys.argv[2], 'w') as output_file:
