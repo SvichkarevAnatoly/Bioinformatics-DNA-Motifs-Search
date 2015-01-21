51a9594ea10d1c2c0577156fa82acc6c87ed3cfa
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-01-22 01:21:59 +0600

    Implemented converting all bed files with intervals to 200 central length interval ucsc format bed files.

--- src/central_range.py
+++ src/central_range.py
@@ -11,17 +11,20 @@ for root, dirs, files in os.walk(directory):
         full_cur_dir = os.path.join(root, cur_dir)
         match_template_list = glob.glob1(full_cur_dir, file_template)
         for match_template in match_template_list:
-            full_match_template_path = os.path.join(full_cur_dir, match_template)
-            bed_file_name_list.append(full_match_template_path)
+            if '_ucsc' not in match_template:
+                full_match_template_path = os.path.join(full_cur_dir, match_template)
+                bed_file_name_list.append(full_match_template_path)
 
 for path_bed_file in bed_file_name_list:
-    diff_list = []
     with open(path_bed_file, 'r') as bed_file:
-        # with open(path_bed_file + '_2', 'w') as bed_converted_file:
-        for interval_line in bed_file:
-            chromosome, start, end = re.split(":|-", interval_line.strip())
-            start = int(start)
-            end = int(end)
-            diff_list.append(end - start)
-            # center = (end + start) / 2
-        print max(diff_list)
+        bed_file_path, bed_file_basename = os.path.split(path_bed_file)
+        bed_file_basename_list = os.path.splitext(bed_file_basename)
+        bed_converted_file_name = os.path.join(bed_file_path,
+                                               bed_file_basename_list[0] + '_ucsc' + bed_file_basename_list[1])
+        with open(bed_converted_file_name, 'w') as bed_converted_file:
+            for interval_line in bed_file:
+                chromosome, start, end = re.split(":|-", interval_line.strip())
+                start = int(start)
+                end = int(end)
+                center = (end + start) / 2
+                bed_converted_file.write(chromosome + ' ' + str(center - 100) + ' ' + str(center + 100) + '\n')
