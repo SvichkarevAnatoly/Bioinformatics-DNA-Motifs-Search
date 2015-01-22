114df59114676f560fc008e2cda2c2bee4aadf51
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-01-22 15:20:03 +0600

    Implemented reverse complement pattern matching and output result.

--- src/my_lib.py
+++ src/my_lib.py
@@ -2,11 +2,15 @@ def searching_result_to_str(sequence, consensus, results, max_score, seq_number)
     if len(results[0]) == 0:
         return ""
     result_str = str(seq_number) + ":\n"
-    result_str += ' ' * 50 + '|' + ' ' * (len(sequence) - 101) + '|' + '\n'
+    # result_str += ' ' * 50 + '|' + ' ' * (len(sequence) - 101) + '|' + '\n'
     result_str += sequence + '\n'
     for result in results:
         for (pos, score) in result:
-            result_str += ' ' * pos + consensus + \
-                          ' ' * (len(sequence) - pos - len(consensus)) + \
+            cur_consensus = consensus
+            if pos <= 0:
+                pos += len(sequence)
+                cur_consensus = consensus.reverse_complement()
+            result_str += ' ' * pos + cur_consensus + \
+                          ' ' * (len(sequence) - pos - len(cur_consensus)) + \
                           " Score: " + str(int(score)) + "/" + str(int(max_score)) + '\n'
     return result_str
--- src/pattern_matching.py
+++ src/pattern_matching.py
@@ -21,14 +21,14 @@ consensus = pwm_matrix.consensus
 pwm_matrix = pwm_matrix.counts
 matrix = [pwm_matrix[n] for n in dna_alf]
 
-output_file = open(sys.argv[3]+"_out.txt", 'w')
+output_file = open(sys.argv[3] + "_out.txt", 'w')
 
 threshold = 0.7 * MOODS.max_score(matrix)
 for i, fasta_seq in enumerate(fasta_seqs):
     sequence = str(fasta_seq.seq)
-    results = MOODS.search(sequence, [matrix], threshold, convert_log_odds=False,
+    results = MOODS.search(sequence, [matrix], threshold, convert_log_odds=False, both_strands=True,
                            pseudocount=0, threshold_from_p=False)
-    result_str = searching_result_to_str(sequence, consensus, results, MOODS.max_score(matrix), i+1)
+    result_str = searching_result_to_str(sequence, consensus, results, MOODS.max_score(matrix), i + 1)
     output_file.write(str(result_str))
 
 output_file.close()
