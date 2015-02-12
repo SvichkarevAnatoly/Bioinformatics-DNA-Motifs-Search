dc0b27cd53ce77c8add1d5470a61c45afc13d03a
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-12 22:46:16 +0600

    Implemented and tested translated interval params to string.

--- src/lib.py
+++ src/lib.py
@@ -8,6 +8,10 @@ def parse_interval_line(interval_line):
     return [chr_name, start, end]
 
 
+def interval_param_to_str(interval_param):
+    return interval_param[0] + ':' + str(interval_param[1]) + '-' + str(interval_param[2])
+
+
 def searching_result_to_str(interval, results, rev_results, sequence_length):
     result_str = ""
     if len(results[0]) == 0 and len(rev_results[0]) == 0:
--- test/test_lib.py
+++ test/test_lib.py
@@ -1,13 +1,21 @@
 import unittest
-from src.lib import parse_interval_line
+import src.lib as lib
 
 
 class Test(unittest.TestCase):
     def test_parse_interval_line(self):
         test_interval_str = "chr1:5-25"
-        actual_interval = parse_interval_line(test_interval_str)
+        actual_interval = lib.parse_interval_line(test_interval_str)
 
         expected_interval = ["chr1", 5, 25]
+
+        self.assertEqual(expected_interval, actual_interval)
+
+    def test_interval_param_to_str(self):
+        test_interval_param = ["chr1", 5, 25]
+        actual_interval = lib.interval_param_to_str(test_interval_param)
+
+        expected_interval = "chr1:5-25"
 
         self.assertEqual(expected_interval, actual_interval)
 
