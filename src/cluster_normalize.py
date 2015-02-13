de867952a5341feab22f58f6bd440a14045ed8c7
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-13 11:32:24 +0600

    Refactored working with intervals.

--- src/cluster_normalize.py
+++ src/cluster_normalize.py
@@ -1,14 +1,14 @@
 import os
-import re
+
+import lib
+
 
 interval_range = 0
 bed_file_name = "../data/clusters/mm10_clusters.bed"
 
 with open(bed_file_name, 'r') as bed_file_tmp:
     for interval_line in bed_file_tmp:
-        chromosome, start, end = re.split(":|-", interval_line.strip())
-        start = int(start)
-        end = int(end)
+        chromosome, start, end = lib.parse_interval_line(interval_line)
         diff = end - start
         interval_range = max(interval_range, diff)
 interval_range += 1
@@ -20,11 +20,8 @@ with open(bed_file_name, 'r') as bed_file:
                                            bed_file_basename_list[0] + '_ucsc' + bed_file_basename_list[1])
     with open(bed_converted_file_name, 'w') as bed_converted_file:
         for interval_line in bed_file:
-            chromosome, start, end = re.split(":|-", interval_line.strip())
-            start = int(start)
-            end = int(end)
+            chromosome, start, end = lib.parse_interval_line(interval_line)
             center = (end + start) / 2
-            new_start = str(center - interval_range / 2)
-            new_end = str(center + interval_range / 2)
-            new_interval_line = chromosome + ' ' + new_start + ' ' + new_end
-            bed_converted_file.write(new_interval_line + '\n')
+            new_start = center - interval_range / 2
+            new_end = center + interval_range / 2
+            bed_converted_file.write(lib.interval_param_to_str(chromosome, new_start, new_end) + '\n')
