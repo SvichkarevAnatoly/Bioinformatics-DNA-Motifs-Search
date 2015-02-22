e02c88fc12b368053acfb1fcbba728f343c209e1
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-22 22:02:35 +0600

    Improved various intervals in bed file for test.

--- test/test_bed_center_extender.py
+++ test/test_bed_center_extender.py
@@ -9,15 +9,16 @@ TEST_DATA_FILENAME = os.path.join(os.path.dirname(__file__), 'test_data/test_bed
 
 class Test(unittest.TestCase):
     def setUp(self):
-        super(Test, self).setUp()
         self.test_data = open(TEST_DATA_FILENAME, 'r')
 
     def test_on_bed_file(self):
         args_list = [self.test_data, 500, None]
         bed_interval_param_list = bce.bed_center_extender(args_list)
 
-        expected_interval = ["chr1", 1800, 2300]
-        self.assertEqual(expected_interval, bed_interval_param_list[0])
+        expected_interval_list = [["chr1", 1800, 2300],
+                                  ['chr9', 1755, 2255],
+                                  ['chr2', 2250, 2750]]
+        self.assertItemsEqual(expected_interval_list, bed_interval_param_list)
 
     def tearDown(self):
         self.test_data.close()
