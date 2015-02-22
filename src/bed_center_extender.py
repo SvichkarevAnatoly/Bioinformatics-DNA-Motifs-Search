815f6e5e3f049204e0bbca121a73a11f365af0fe
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-23 00:53:09 +0600

    bed_center_extender: refactored for testing command line args.

--- src/bed_center_extender.py
+++ src/bed_center_extender.py
@@ -2,14 +2,14 @@ import argparse
 import lib
 
 
-def parse_args():
+def create_parser():
     parser = argparse.ArgumentParser(description="Resulting intervals in the input file to the same length")
     parser.add_argument("bedfile", type=argparse.FileType('r'), help="file with bed format intervals")
     parser.add_argument("-l", dest="length", type=int, metavar='length', help="common extended length")
     # TODO: remove outfile if args error
     parser.add_argument("-o", dest="outfile", type=argparse.FileType('w'), metavar='outfile',
                         help="output file with extended bed format intervals")
-    return parser.parse_args()
+    return parser
 
 
 def interval_center_extender(bed_file, length):
@@ -29,8 +29,7 @@ def write_bed_file(interval_param_list, outfile):
         outfile.write("%s\n" % lib.interval_param_list_to_str(interval_param))
 
 
-if __name__ == "__main__":
-    args = parse_args()
+def workflow(args):
     bedfile = args.bedfile
     extended_interval_list = interval_center_extender(bedfile, args.length)
     output_file = args.outfile
@@ -38,3 +37,13 @@ if __name__ == "__main__":
         output_file = lib.create_output_file_name(bedfile.name)
         output_file = open(output_file, 'w')
     write_bed_file(extended_interval_list, output_file)
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    workflow(args)
+
+
+if __name__ == "__main__":
+    main()
