3f742168f33478e4ca66e08ffe0c55bf9a153d43
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-24 15:43:28 +0600

    pattern_matching.py: implemented search algorithm.

--- src/pattern_matching.py
+++ src/pattern_matching.py
@@ -1,6 +1,8 @@
 import argparse
+import re
 from Bio import SeqIO
 import Bio.motifs as motifs
+import MOODS
 import lib
 
 
@@ -13,18 +15,30 @@ def create_parser():
 
     parser.add_argument("-tf", "--factor", dest="tf", type=int, metavar='tf',
                         help="transcription factor name in pwm file. "
-                        "If not specified, matching with all tf in pwm file.")
+                             "If not specified, matching with all tf in pwm file.")
     parser.add_argument("-th", "--threshold", dest="threshold", type=float, default=0.7, metavar='threshold',
                         help="The parameter threshold split for better control on what parts of the scoring are used. "
-                        "If not specified, threshold=0.7.")
+                             "If not specified, threshold=0.7.")
     return parser
 
 
 def process(args):
     # TODO: write triggers in parser for writing input data
-    seq_list = list(SeqIO.parse(args.fasta, "fasta"))
+    seqs = list(SeqIO.parse(args.fasta, "fasta"))
     pwm_record_list = motifs.parse(args.pwm, "TRANSFAC")
     matrices = lib.create_matrices_from_pwms(pwm_record_list, args.tf)
+
+    threshold = args.threshold * MOODS.max_score(matrices)
+    for seq in seqs:
+        sequence = str(seq.seq)
+        # TODO: created lib method
+        interval = re.split("=| ", seq.description)[2]
+        results = MOODS.search(sequence, matrices, threshold, convert_log_odds=False, both_strands=True, pseudocount=0,
+                               threshold_from_p=False)
+
+        reversed_sequence = seq.seq[::-1]
+        reversed_results = MOODS.search(reversed_sequence, matrices, threshold, convert_log_odds=False,
+                                        both_strands=True, pseudocount=0, threshold_from_p=False)
 
     return None
 
