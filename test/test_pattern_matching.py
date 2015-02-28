a9358956c96b5babdb9c005419fbf8e988e53d9d
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-02-28 23:10:59 +0600

    test_pattern_matching.py: test saving results to file.

--- test/test_pattern_matching.py
+++ test/test_pattern_matching.py
@@ -6,6 +6,7 @@ import src.pattern_matching as pm
 
 TEST_FASTA_FILENAME = os.path.join(os.path.dirname(__file__), "test_data/fasta.fa")
 TEST_PWM_FILENAME = os.path.join(os.path.dirname(__file__), "test_data/pwms_transfac.dat")
+TEST_OUT_FILENAME = os.path.join(os.path.dirname(__file__), "test_data/output.dat")
 
 
 def silent_remove(file_name):
@@ -56,6 +57,28 @@ class Test(unittest.TestCase):
         expected_pos = (0, 6.0)
         seq1_tf1_positions = seq1_tf1_result[1]
         self.assertEqual(expected_pos, seq1_tf1_positions[0])
+
+    def test_output_file_result(self):
+        self.args = self.parser.parse_args([str(TEST_FASTA_FILENAME),
+                                            str(TEST_PWM_FILENAME),
+                                            "-o",
+                                            str(TEST_OUT_FILENAME)])
+        result = pm.process(self.args)
+        pm.save(result, self.args)
+
+        expected_output = '\n'.join([
+            ">seq1",
+            "motif1 0;3;6;11;12;15;16;17;19;28;34;41;48;49;53;58;59;62;65",
+            "motif2 10;17;19;28;35;43;53;61;67",
+            ">seq2",
+            "motif1 0;1;6;7;15;19;20;28;31;32;36;44;45;46",
+            "motif2 0;1;2;9;13;34;42",
+        ]) + '\n'
+        with open(TEST_OUT_FILENAME, 'r') as output_file:
+            actual_output = output_file.read()
+
+        self.assertEqual(expected_output, actual_output)
+        os.remove(TEST_OUT_FILENAME)
 
     def tearDown(self):
         super(Test, self).tearDown()
