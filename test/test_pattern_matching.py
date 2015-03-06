243f7b541fc050da160b66747605e4ad8f1464b3
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-06 15:20:24 +0600

    test_pattern_matching.py: fixed test_run_on_test_data.

--- test/test_pattern_matching.py
+++ test/test_pattern_matching.py
@@ -1,8 +1,10 @@
 import os
 import unittest
 import errno
+import cStringIO
 
 import src.pattern_matching as pm
+
 
 TEST_FASTA_FILENAME = os.path.join(os.path.dirname(__file__), "test_data/fasta.fa")
 TEST_PWM_FILENAME = os.path.join(os.path.dirname(__file__), "test_data/pwms_transfac.dat")
@@ -36,7 +38,7 @@ class Test(unittest.TestCase):
         self.args.pwm.close()
 
     def test_reversed_flag(self):
-        args = [str(TEST_FASTA_FILENAME), str(TEST_PWM_FILENAME)]
+        args = [str(TEST_FASTA_FILENAME), str(TEST_PWM_FILENAME), "-tf", "motif1"]
         self.args = self.parser.parse_args(args)
         self.assertFalse(self.args.reversed)
 
@@ -44,19 +46,38 @@ class Test(unittest.TestCase):
         self.args = self.parser.parse_args(args)
         self.assertTrue(self.args.reversed)
 
+        tempfile = cStringIO.StringIO()
+        self.args.output = tempfile
+
+        result = pm.process(self.args)
+        pm.save(result, self.args)
+
+        tempfile.seek(0)
+        result = tempfile.read()
+
+        expected = "\n".join([
+            ">seq1",
+            "motif1 0;3;6;11;12;15;16;17;19;28;34;41;48;49;53;58;59;62;65",
+            ">seq2",
+            "motif1 0;1;6;7;15;19;20;28;31;32;36;44;45;46",
+        ])
+        self.assertEqual(result, expected)
+
     def test_run_on_test_data(self):
         self.args = self.parser.parse_args([str(TEST_FASTA_FILENAME), str(TEST_PWM_FILENAME)])
         result = pm.process(self.args)
 
         expected_seq_number = 2
         self.assertEqual(expected_seq_number, len(result))
-        seq1_result = result[0]
-        self.assertEqual("seq1", seq1_result[0])
-        seq1_tf1_result = seq1_result[1][0]
-        self.assertEqual("motif1", seq1_tf1_result[0])
-        expected_pos = (0, 6.0)
-        seq1_tf1_positions = seq1_tf1_result[1]
-        self.assertEqual(expected_pos, seq1_tf1_positions[0])
+        seq_result = result[0]
+        self.assertEqual("seq1", seq_result.seq_name)
+        expected_tf = "motif1"
+        self.assertEqual(expected_tf, seq_result.tfs[0])
+        self.assertIsNotNone(seq_result.tf_dict[expected_tf])
+
+        expected_first_match = (0, 6.0)
+        actual_matching_list = seq_result.tf_dict[expected_tf].directed
+        self.assertEqual(expected_first_match, actual_matching_list[0])
 
     def test_output_file_result(self):
         self.args = self.parser.parse_args([str(TEST_FASTA_FILENAME),
