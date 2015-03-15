3530d7d9ba4e28f05caa46e05f2ecd9f3cd3199a
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-15 19:32:10 +0600

    test(test_lib.py) test generate pwm str method

--- test/test_lib.py
+++ test/test_lib.py
@@ -208,5 +208,18 @@ class TestLib(unittest.TestCase):
         self.assertEqual(2, lib.to_ind('G'))
         self.assertEqual(3, lib.to_ind('T'))
 
+    def test_generate_pwm_str(self):
+        expected = '\n'.join([
+            "ID motif1",
+            "P0  A C G T",
+            "1   9 0 0 0",
+            "2   0 9 0 0",
+            "3   0 0 9 0",
+            "4   0 0 0 9",
+            "//"
+        ]) + '\n'
+        actual = lib.generate_pwm_str("motif1", "ACGT")
+        self.assertEqual(expected, actual)
+
 if __name__ == "__main__":
     unittest.main()
