4c1f4ebe31e41e4cf10336ffd1d88ec8189cb347
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-24 11:19:07 +0600

    feature(test_pwm_generator.py) create args method

--- test/test_pwm_generator.py
+++ test/test_pwm_generator.py
@@ -1,7 +1,21 @@
+from argparse import Namespace
 import unittest
+import cStringIO
 
 
 class Test(unittest.TestCase):
+    def create_seqs_handler(self, sequences):
+        seqs_handler = cStringIO.StringIO()
+        seqs_handler.write('\n'.join(sequences)+'\n')
+        seqs_handler.close()
+        return seqs_handler
+
+    def create_args(self, sequences):
+        args = Namespace()
+        args.seqs = self.create_seqs_handler(sequences)
+        args.output = cStringIO.StringIO()
+        return args
+
     def test_creating_pwm(self):
         pass
 
