35cd2a172b901461086738d2677ad79872ea8f6f
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-24 13:05:06 +0600

    refactor(lib.py) delete searching_result_to_str

--- src/lib.py
+++ src/lib.py
@@ -108,31 +108,6 @@ def get_join_position_str(positions, seq_length):
     return ';'.join(positions_str)
 
 
-def searching_result_to_str(interval, results, rev_results, sequence_length):
-    result_str = ""
-    if len(results[0]) == 0 and len(rev_results[0]) == 0:
-        return result_str
-    else:
-        result_str += ">" + interval + '\n'
-        for result in results:
-            pos_str = ""
-            for (pos, score) in result:
-                if pos < 0:
-                    pos_str += '-'
-                    pos += sequence_length
-                pos_str += str(pos) + ' '
-            result_str += pos_str + '\n'
-        for result in rev_results:
-            pos_str = ""
-            for (pos, score) in result:
-                if pos < 0:
-                    pos_str += '-'
-                    pos += sequence_length
-                pos_str += str(pos) + ' '
-            result_str += pos_str + '\n'
-    return result_str
-
-
 def reverse_complement(sequence):
     return str(Seq(sequence).reverse_complement())
 
