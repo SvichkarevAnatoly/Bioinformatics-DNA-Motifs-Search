20960ca0881a14342a7f610854f5859974cc3ffe
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-24 10:51:59 +0600

    feature(pwm_generator.py) parse input seqs

--- src/utils/pwm_generator.py
+++ src/utils/pwm_generator.py
@@ -2,10 +2,28 @@ import argparse
 from signal import signal, SIGPIPE, SIG_DFL
 import sys
 
+from Bio.Alphabet import IUPAC
+from Bio import Alphabet
+from Bio.Seq import Seq
+
+
+class ReadSeqAction(argparse.Action):
+    def __call__(self, parser, args, seqs_handler, option_string=None):
+        seqs = []
+        for seq_line in seqs_handler:
+            seq_string = seq_line.strip()
+            seq = Seq(seq_string, IUPAC.unambiguous_dna)
+            Alphabet._verify_alphabet(seq)
+            # TODO: check same length
+            seqs.append(seq)
+        seqs_handler.close()
+        setattr(args, self.dest, seqs)
+
 
 def create_parser():
     parser = argparse.ArgumentParser(description="TODO")
     parser.add_argument("seqs", type=argparse.FileType('r'),
+                        action=ReadSeqAction,
                         help="TODO")
     parser.add_argument("-o", "--output", nargs='?', dest="output",
                         type=argparse.FileType('w'), default=sys.stdout,
