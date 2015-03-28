3afb6019251e560c3ea55f2bc5e0416f658629d8
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-03-28 22:21:39 +0600

    refactor(test_pwm_generator.py) delete create_seqs

--- test/test_pwm_generator.py
+++ test/test_pwm_generator.py
@@ -1,21 +1,16 @@
 from argparse import Namespace
 import unittest
 import cStringIO
-
-from Bio.Alphabet import IUPAC
-from Bio.Seq import Seq
+import lib
 
 import suite
 import utils.pwm_generator as pg
 
 
 class Test(unittest.TestCase):
-    def create_seqs(self, sequences):
-        return [Seq(seq, IUPAC.unambiguous_dna) for seq in sequences]
-
     def create_args(self, sequences, motif_name):
         args = Namespace()
-        args.seqs = self.create_seqs(sequences)
+        args.seqs = lib.biopython_seqs(sequences)
         args.output = cStringIO.StringIO()
         args.motif = motif_name
         return args
