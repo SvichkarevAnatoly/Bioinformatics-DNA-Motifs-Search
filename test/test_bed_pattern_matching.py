0bda6187eaad8d66f8cd0f3654d99bd4f6ada62c
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-04-14 21:29:18 +0600

    test(test_bed_pattern_matching.py) bed parser

--- test/test_bed_pattern_matching.py
+++ test/test_bed_pattern_matching.py
@@ -29,7 +29,21 @@ class Test(unittest.TestCase):
             "-rc",
             "-c"
         ])
+        # TODO
 
+    def test_read_bed_file(self):
+        args = self.parser.parse_args([
+            str(self.fasta_file_path),
+            str(self.pfm_file_path),
+            str(self.bed_file_path)
+        ])
+
+        bed_peaks = args.bed
+        expected_peak1 = ("chr1", "4736010", "4736158", "Z4_Sox2_peak_1")
+        self.assertSequenceEqual(expected_peak1, bed_peaks.next())
+
+        expected_peak2 = ("chr1", "5223047", "5223196", "Z4_Sox2_peak_2")
+        self.assertSequenceEqual(expected_peak2, bed_peaks.next())
 
 if __name__ == "__main__":
     unittest.main()
