7423b9dca9496e43e39e008285fae59fbf7a31a3
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-04-14 19:49:25 +0600

    refactor(test_track_lib.py) bed file fields

--- test/test_track_lib.py
+++ test/test_track_lib.py
@@ -7,13 +7,15 @@ from bbcflib.track import track
 class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.bed_file_path = os.path.join(os.path.dirname(__file__), "../data_samples/analysis/csv_like.bed")
+        cls.bed_file_path = os.path.join(os.path.dirname(__file__),
+                                         "../data_samples/analysis/csv_like.bed")
+        cls.bed_fields = ['chrom', 'chromStart', 'chromEnd',
+                          'c1', 'c2', 'c3', 'c4', 'c5', 'c6',
+                          'peakName']
 
     def test_track_fields(self):
-        t = track(self.bed_file_path, format='txt', separator='\t',
-                  fields=['chrom', 'chromStart', 'chromEnd',
-                          'c1', 'c2', 'c3', 'c4', 'c5', 'c6',
-                          'peakName'])
+        t = track(self.bed_file_path, format='txt',
+                  separator='\t', fields=self.bed_fields)
 
         s = t.read(fields=['chrom', 'chromStart', 'chromEnd', 'peakName'])
 
